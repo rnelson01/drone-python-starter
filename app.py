9b4fc5a7aa76f4d063b3405d43f22260737114a8
Ryan Nelson <44874266+rnelson01@users.noreply.github.com>
2020-09-16 11:12:09 -0500

    Update app.py
    new message

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/', methods = ['GET'])
 def home():
 
-    return "Drone.io is Awesome"
+    return "Drone.io is Awesome!"
 
 if __name__ == '__main__':
     #app.run(debug=False)
